5c69de2c441b666353d4f1fc5ad0d2e402830a94
rafeed035 <shahed.rafeed@gmail.com>
2022-09-17 23:18:11 +0600

    executor added

--- src/main/java/Executor.java
+++ src/main/java/Executor.java
@@ -8,6 +8,7 @@ import RepositoryImplementation.ProductRepositoryImplementation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Scanner;
 
@@ -60,28 +61,134 @@ public class Executor {
                 "\n16. Exit");
 
         System.out.println("\n=================Your Choice=================");
+        System.out.println("\nYour Choice: ");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
             switch (choice){
+                //view all categories
                 case 1:
                     for(int i = 0; i < categories.size(); i++){
                         System.out.println(categories.get(i).getCategoryId() + " " + categories.get(i).getCategoryName());
                     }
                     break;
 
+                    //view a category
                 case 2:
                     System.out.println("Enter Category Id: ");
                     int choiceCategory = scanner.nextInt();
                     Category category = categoryRepositoryImplementation.getCategory(choiceCategory);
                     System.out.println(category.getCategoryId() + " " + category.getCategoryName());
+                    break;
+
+                    //add a new category
+                case 3:
+                    System.out.println("Enter Category Id: ");
+                    int categoryId = scanner.nextInt();
+                    System.out.println("Enter Category Name: ");
+                    String categoryName = scanner.next();
+                    scanner.nextLine();
+                    Category categoryNew = new Category(categoryId, categoryName);
+                    int insertCategory = categoryRepositoryImplementation.insertCategory(categoryNew);
+                    System.out.println("New category Inserted with id = " + insertCategory);
+                    break;
+
+                    //update a category
                 case 4:
+                    System.out.println("Enter Category Id: ");
+                    int categoryUpdateId = scanner.nextInt();
+                    System.out.println("Enter Category Name: ");
+                    String categoryUpdateName = scanner.next();
+                    scanner.nextLine();
+                    Category categoryUpdate = new Category(categoryUpdateId, categoryUpdateName);
+                    int updateCategory = categoryRepositoryImplementation.updateCategory(categoryUpdate);
+                    System.out.println("Updated category with id = " + updateCategory);
+                    break;
+
+                    //delete a category
                 case 5:
+                    System.out.println("Enter Category Id: ");
+                    int categoryDeleteId = scanner.nextInt();
+                    scanner.nextLine();
+                    int deleteCategory = categoryRepositoryImplementation.deleteCategory(categoryDeleteId);
+                    System.out.println("Deleted category with id = " + deleteCategory);
+                    break;
+
+                    //brands
+                //view all brands
                 case 6:
+                    for(int i = 0; i < brands.size(); i++){
+                        Category brandCategory = categoryRepositoryImplementation.getCategory(brands.get(i).getCategory_id());
+                        String allBrandCategoryName = brandCategory.getCategoryName();
+                        System.out.println(brands.get(i).getBrandId() +
+                                " " + brands.get(i).getBrandName() +
+                                " Category : " + allBrandCategoryName);
+                    }
+                    break;
+
+                    //view a particular brand
                 case 7:
+                    System.out.println("Enter Brand Id: ");
+                    int brandId = scanner.nextInt();
+                    Brand brand = brandRepositoryImplementation.getBrand(brandId);
+                    Category brandCategory = categoryRepositoryImplementation.getCategory(brand.getCategory_id());
+                    String brandCategoryName = brandCategory.getCategoryName();
+                    System.out.println(brand.getBrandId() + " " +
+                            brand.getBrandName() +
+                            " " +
+                            "Category : " + brandCategoryName);
+                    break;
+
+                    //add a new brand
                 case 8:
+                    System.out.println("Enter Brand Id: ");
+                    int newBrandId = scanner.nextInt();
+                    System.out.println("Enter Brand Name: ");
+                    String newBrandName = scanner.next();
+                    scanner.nextLine();
+                    int brandCategoryId = scanner.nextInt();
+                    scanner.nextLine();
+                    Brand brandNew = new Brand(newBrandId, newBrandName, brandCategoryId);
+                    int brandInsertResult = brandRepositoryImplementation.insertBrand(brandNew);
+                    System.out.println("New Brand Inserted with id = " + brandInsertResult);
+                    break;
+
+                    //update a new brand
                 case 9:
+                    System.out.println("Enter Brand Id: ");
+                    int updateBrandId = scanner.nextInt();
+                    System.out.println("Enter Brand Name: ");
+                    String updateBrandName = scanner.next();
+                    scanner.nextLine();
+                    int updatebrandCategoryId = scanner.nextInt();
+                    scanner.nextLine();
+                    Brand brandUpdate = new Brand(updateBrandId, updateBrandName, updatebrandCategoryId);
+                    int brandUpdateResult = brandRepositoryImplementation.updateBrand(brandUpdate);
+                    System.out.println("Brand updated with id = " + brandUpdateResult);
+                    break;
+
+                    // delete a brand
                 case 10:
+                    System.out.println("Enter Brand Id: ");
+                    int brandDeleteId = scanner.nextInt();
+                    scanner.nextLine();
+                    int deleteBrandResult = brandRepositoryImplementation.deleteBrand(brandDeleteId);
+                    System.out.println("Deleted category with id = " + deleteBrandResult);
+                    break;
+
+                    //view all products
                 case 11:
+                    for(int i = 0; i < products.size(); i++){
+                        Category productCategory = categoryRepositoryImplementation.getCategory(products.get(i).getCategoryId());
+                        String allProductCategoryName = productCategory.getCategoryName();
+                        Brand productBrand = brandRepositoryImplementation.getBrand(products.get(i).getBrandId());
+                        String allProductBrandName = productBrand.getBrandName();
+                        System.out.println(products.get(i).getProductId() +
+                                " " + products.get(i).getProductName() +
+                                " Price : " + products.get(i).getProductPrice() +
+                                " Category : " + allProductCategoryName +
+                                " Brand : " + allProductBrandName);
+                    }
+                    break;
                 case 12:
                 case 13:
                 case 14:
